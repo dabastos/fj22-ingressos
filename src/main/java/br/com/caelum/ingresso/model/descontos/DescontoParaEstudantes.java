44758246dab438c8dbf1425cf4c84dccc9bd7540
djalmabastos1953@gmail.com <lab7545@caelum133-15>
2018-03-16 22:47:45 -0300

    arquivos do curso

---
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaEstudantes.java
@@ -0,0 +1,18 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import java.math.BigDecimal;
+
+public class DescontoParaEstudantes implements Desconto {
+	
+	@Override
+	public String getDescricao () {
+		return "Dsconto Estudante";
+	}
+
+	@Override
+	public BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
